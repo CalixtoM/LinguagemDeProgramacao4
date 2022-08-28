50d2785ae6cdff7409bd9c5cc328892980d4c5ed
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2022-08-27 23:26:43 -0300

    Aula 04 - tipos de dados

---
+++ Aula04/tipoDados1.php
@@ -0,0 +1,23 @@
+<?php 
+// Tipo de Dados
+// =============
+
+# As variaveis podem ser de diferentes tipos de dados
+# A mesma variavel pode conter tipos de dados diferentes
+
+# Atribuições de tipos diferentes para a mesma variavel 
+$variavel = 100;
+$variavel = "cem";
+
+# Apesar das variaveis serem de tipo implicito
+# é importante conhecer alguns tipos de variaveis
+
+$inteiro = 100;             # Valores seguidos sem casas decimais
+$float = 10.5;              # Valores seguidos de casas decimais
+$bool = true;               # Assume valores falso ou verdadeiro
+$string = "Bom dia";        # Variavel de texto com comportamento de uma cadeia de caracteres
+$array = [1, 2, 3];         # Variavel com coleção de valores
+$nulo = null;               # Variavel com valor nulo
+// $pessoa = new Pessoa();  # Objeto com propriedade e metodos
+
+?>
