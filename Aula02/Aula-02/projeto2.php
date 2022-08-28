6530081137de039cdd85fb916c56d23f1bf7eecf
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2022-08-27 23:22:30 -0300

    Aula 02

---
+++ Aula02/Aula-02/projeto2.php
@@ -0,0 +1,46 @@
+<?php 
+
+    echo "<h1>Aulas de LP4</h1>";
+    echo "<h2>Sexta-Feira, 13h10</h2>";
+    echo "<h3>Professor Jonatas<h3>";
+    echo "<br>";
+
+    #  Tipo de comentario 1
+    /* Tipo de comentario 2 */
+    // Tipo de comentario 3
+    echo "<div class='linha'></div>";
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Projeto 1</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+
+
+</body>
+</html>
+
+<style>
+
+    h1, h2, h3{
+        color: blue;
+        margin-bottom: -20px;
+    }
+
+    /*body{
+        background-image: linear-gradient(to bottom right, lightgreen, purple);
+    }*/
+
+    .linha{
+        width: 100%;
+        height: 100%;
+    }
+</style>
