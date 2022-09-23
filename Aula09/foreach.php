7c6cdb484f6c72478f6ff9f13332e7b185c6f507
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2022-09-23 16:25:34 -0300

    aula 09 final

--- Aula09/foreach.php
+++ Aula09/foreach.php
@@ -4,7 +4,7 @@
 # Notar que não há necessidade de inicializar uma variavel, nem realizar o incremento ou decremento desta variavel,
 # Poi, o ciclo passa automaticamente de forma sequencial em cada elemento do "array"
 
-# Note também que cada elemento do "array" e carregado na variavel logo após a declaração de "AS"
+# Note também que cada elemento do "array" e carregado na variavel logo após a declaração de
 
 # Exemplo 1
 $nomes = ["Tomé", "André", "Tadeu"];
@@ -13,5 +13,31 @@ foreach($nomes as $nome){
     echo "<br>";
 }
 
+echo "<br><br>";
 
+# Exemplo 2
+$x = [1, 2, 3];
+
+foreach($x as $y){
+    echo $y . "<br>";
+
+}
+
+echo "<br><br>";
+
+# Exemplo 3
+echo "Exemplo 3 <br>";
+# Existe uma assinatura diferente para o FOREACH
+$EstadoCapital=[
+    'Acre' => 'Rio Branco',
+    'Amapa' => 'Macapa',
+    'Alagoas' => 'Maceio',
+    'Ceara' => 'Fortaleza'
+];
+
+foreach($EstadoCapital as $chave => $value){
+    echo "A capital do estado " . $chave . " é " . $value;
+    echo "<br>";
+
+}
 ?>
